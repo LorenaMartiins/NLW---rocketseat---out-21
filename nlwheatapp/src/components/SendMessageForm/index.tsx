d1407df64e5c4e98b247ade62956a42ac516f99a
LorenaMartiins <lorena.martins.farias@hotmail.com>
2021-12-01 14:31:33 -0300

    NLW

---
+++ nlwheatapp/src/components/SendMessageForm/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Alert, TextInput, Keyboard, View } from "react-native";
+import { Button } from "../../components/Button";
+import { api } from "../../services/api";
+import { COLORS } from "../../theme";
+
+import { styles } from "./styles";
+
+export function SendMessageForm() {
+  const [message, setMessage] = useState("");
+  const [sendingMessage, setSendingMessage] = useState(false);
+
+  async function handleMessageSubmit() {
+    const messageFormatted = message.trim();
+
+    if (messageFormatted.length > 0) {
+      setSendingMessage(true);
+      await api.post("/messages", { message: messageFormatted });
+
+      setMessage("");
+      Keyboard.dismiss();
+      setSendingMessage(false);
+      Alert.alert("Mensagem enviada com sucesso!");
+    } else {
+      Alert.alert("Escreva a mensagem para enviar.");
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Qual sua expectativa para o evento"
+        placeholderTextColor={COLORS.GRAY_PRIMARY}
+        multiline
+        maxLength={140}
+        onChangeText={setMessage}
+        value={message}
+        keyboardAppearance="dark"
+        editable={!sendingMessage}
+      />
+
+      <Button
+        title="ENVIAR MENSAGEM"
+        backgroundColor={COLORS.PINK}
+        color={COLORS.WHITE}
+        isLoading={sendingMessage}
+        onPress={handleMessageSubmit}
+      />
+    </View>
+  );
+}
